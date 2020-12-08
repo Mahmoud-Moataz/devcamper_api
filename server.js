533e7d41df1c4c2431023e7b7ebe3bafa05ed5e4
mahmoudmoataz24 <mahmoud.moatazz24@gmail.com>
2020-12-08 20:56:21 +0200

    add morgan and custome logger middlware

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const morgan = require('morgan');
 //Route files
 const bootcamps = require('./routes/bootcamps');
 
 //load env vars
 dotenv.config({ path: './config/config.env' });
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
-  next();
-};
-
 const app = express();
 
-app.use(logger);
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
