5d3d926ba93b74608355b85c39fc136c757f8cdf
mahmoudmoataz24 <mahmoud.moatazz24@gmail.com>
2020-12-19 20:37:35 +0200

    complete slug & location fields

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const { default: slugify } = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -32,6 +34,10 @@ const BootcampSchema = new mongoose.Schema({
   email: {
     type: String,
     match: [/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/, 'enter a valid email'],
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add an address'],
   },
   location: {
     //GeoJSON Point
@@ -86,6 +92,31 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+//Create bootcamp slug from the name
+BootcampSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+//Geocode & create location field
+BootcampSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    tepe: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zibCode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  //Do not save address field in DB because we replace it with 'formattedAddress' in location field
+  this.address = undefined;
+  next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
