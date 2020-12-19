b275eeb7e4456880917bea40346896f5077795bd
mahmoudmoataz24 <mahmoud.moatazz24@gmail.com>
2020-12-19 18:15:58 +0200

    check error types in custom error handler

--- middleware/error.js
+++ middleware/error.js
@@ -1,9 +1,35 @@
-const errorHandler = (err, req, res, next) => {
-  console.log(err.stack.red);
+const ErrorResponse = require('../utils/errorResponse');
 
-  res.status(500).json({
+const errorHandler = (err, req, res, next) => {
+  // eslint-disable-next-line node/no-unsupported-features/es-syntax
+  let error = { ...err };
+
+  //I think object clone copy just original class members and not the parent members of the cloned class so we write the next line
+  error.message = err.message;
+
+  //log to console for dev
+  console.log(err);
+
+  //mongoose bad ObjectId
+  if (err.name === 'CastError') {
+    const message = `Resource not found with id of ${err.value} `;
+    error = new ErrorResponse(message, 404);
+  }
+
+  //mongoose duplicate key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  //mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+  res.status(error.statusCode || 500).json({
     success: false,
-    error: err.message,
+    error: error.message || 'Server Error',
   });
 };
 
