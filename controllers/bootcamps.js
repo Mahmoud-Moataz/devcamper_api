b163faeda0914686c461ee14df99b50a58728b33
mahmoudmoataz24 <mahmoud.moatazz24@gmail.com>
2020-12-24 00:07:44 +0200

    add photo upload

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const ErrorResponse = require('../utils/errorResponse');
 const Bootcamp = require('../models/Bootcamp');
 const asyncHandler = require('../middleware/asyncHandler');
@@ -152,5 +153,44 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
     success: true,
     count: bootcamps.length,
     data: bootcamps,
+  });
+});
+
+//@desc     upload photo for bootcamp
+//@route    PUT /api/v1/bootcamps/:id/photo
+//@access   private
+exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
+  if (!bootcamp) {
+    return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id} `, 404));
+  }
+
+  if (!req.files) {
+    return next(new ErrorResponse(`please upload a file`, 400));
+  }
+  const file = req.files.file;
+
+  //make sure the file is a photo
+  if (!file.mimetype.startsWith('image')) {
+    return next(new ErrorResponse(`please upload an image file`, 400));
+  }
+
+  if (file.size > process.env.MAX_FILE_UPLOAD) {
+    return next(
+      new ErrorResponse(`please upload an image less than ${process.env.MAX_FILE_UPLOAD}`, 500)
+    );
+  }
+
+  //create uniq custome file name
+  //path part to get file extension
+  file.name = `photo_${bootcamp._id}${path.parse(file.name).ext}`;
+
+  file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async (err) => {
+    if (err) {
+      console.log(err);
+      return next(new ErrorResponse(`proplem with file upload`, 500));
+    }
+    await Bootcamp.findByIdAndUpdate(req.params.id, { photo: file.name });
+    res.status(200).json({ success: true, data: file.name });
   });
 });
